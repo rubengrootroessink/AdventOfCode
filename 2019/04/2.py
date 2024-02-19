650cec851ceab483a47bcefb44617957bf4e673f
s1468642 <ruben331@gmail.com>
2024-02-19 23:21:41 +0100

    Added 2019, Day 04

---
+++ 2019/04/2.py
@@ -0,0 +1,16 @@
+# Inspired by others
+from collections import Counter
+
+with open('input.txt') as f:
+    low, high = [int(x) for x in f.read().strip().split('-')]
+
+count = 0
+for num in range(low, high+1):
+    text = str(num)
+    
+    if text == ''.join(sorted(text)):
+        c = set(Counter(text).values())
+        if bool(c & {2}):
+            count += 1
+
+print(count)
