768a49b1ab7686c481c0460eb4aa464be34fe80f
s1468642 <ruben331@gmail.com>
2024-02-19 09:35:44 +0100

    Added 2019, Day 2

---
+++ 2019/02/1.py
@@ -0,0 +1,39 @@
+import copy
+
+def run(prog_instrs, noun, verb):
+    prog = copy.deepcopy(prog_instrs)
+    prog[1] = noun
+    prog[2] = verb
+
+    eip = 0
+
+    finished = False
+    while not finished:
+        op_code = prog[eip]
+
+        match op_code:
+            case 1:
+                input_1, input_2, output = prog[eip+1:eip+4]
+                prog[output] = prog[input_1] + prog[input_2]
+                eip += 4
+
+            case 2:
+                input_1, input_2, output = prog[eip+1:eip+4]
+                prog[output] = prog[input_1] * prog[input_2]
+                eip += 4
+
+            case 99:
+                finished = True
+
+    if prog[0] == 19690720:
+        print(str(noun) + str(verb))
+        return True
+    else:
+        return False
+
+with open('input.txt') as f:
+    prog = [int(x) for x in f.read().strip().split(',')]
+
+for i in range(0, len(prog)):
+    for j in range(0, len(prog)):
+        run(prog, i, j)
