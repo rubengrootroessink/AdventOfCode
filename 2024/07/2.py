c8255f4d74b93de5ff8b5fcb7b992c2a9f7ff025
s1468642 <ruben331@gmail.com>
2024-12-07 20:57:11 +0100

    Updated 2024, Day 07

--- 2024/07/2.py
+++ 2024/07/2.py
@@ -1,35 +1,28 @@
-from itertools import product
+import math
 
 with open('input.txt') as f:
     lines = [[int(y) for y in x.strip().replace(':', '').split(' ')] for x in f.readlines()]
+
+def rec(final_val, curr_val, vals):
+    if curr_val > final_val:
+        return False
+    elif final_val == curr_val and vals == []:
+        return True
+    elif vals == []:
+        return False
+    else:
+        new_vals = []
+        new_vals.append(curr_val + vals[0])
+        new_vals.append(curr_val * vals[0])
+        new_vals.append(curr_val * 10**int(math.log10(vals[0]) + 1) + vals[0])
+        return any([rec(final_val, new_val, vals[1:]) for new_val in new_vals])
 
 cal_result = 0
 for line in lines:
     result = line[0]
     values = line[1:]
-    options = product(['*', '+', '||'], repeat=len(values)-1)
 
-    matches = False
-    for option in options:
-        ops = ['*'] + list(option)
-        
-        tmp_result = 1
-        for i, val in enumerate(values):
-            if ops[i] == '*':
-                tmp_result *= val
-            elif ops[i] == '+':
-                tmp_result += val
-            elif ops[i] == '||':
-                tmp_result = int(str(tmp_result) + str(val))
-
-            if tmp_result > result:
-                break
-
-        if tmp_result == result:
-            matches = True
-            break
-
-    if matches:
+    if rec(result, 0, values):
         cal_result += result
 
 print(cal_result)
