4c502381c40f722261fe508d2fa90a1db99d7e0e
s1468642 <ruben331@gmail.com>
2024-12-13 14:52:47 +0100

    Added 2024, Day 13

---
+++ 2024/13/2.py
@@ -0,0 +1,24 @@
+with open('input.txt') as f:
+    blocks = f.read().strip().split('\n\n')
+
+count = 0
+for block in blocks:
+
+    a, b, p = block.split('\n')
+    ax = float(a.split('A: X+')[1].split(',')[0])
+    ay = float(a.split('Y+')[1])
+    bx = float(b.split('B: X+')[1].split(',')[0])
+    by = float(b.split('Y+')[1])
+    px = float(int(p.split('X=')[1].split(',')[0])+10000000000000)
+    py = float(int(p.split('Y=')[1])+10000000000000)
+
+    n = int((bx * py - by * px ) / (bx * ay - by * ax))
+    m = int((px - ax * n) / bx)
+
+    eq_1 = ax * n + bx * m == px
+    eq_2 = ay * n + by * m == py
+
+    if eq_1 and eq_2:
+        count +=  n*3 + m
+
+print(count)
