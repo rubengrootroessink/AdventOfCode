c6afcbeebfb172b3164f654abde5a217913925c0
s1468642 <ruben331@gmail.com>
2024-12-26 23:03:09 +0100

    Updated 2019, Day 02

--- 2019/02/2.py
+++ 2019/02/2.py
@@ -1,39 +1,47 @@
 import copy
 
-def run(prog_instrs, noun, verb):
-    prog = copy.deepcopy(prog_instrs)
-    prog[1] = noun
-    prog[2] = verb
-
+def run(instrs, pos_1, pos_2):
     eip = 0
-
+    instrs[1] = pos_1
+    instrs[2] = pos_2
+    
     finished = False
     while not finished:
-        op_code = prog[eip]
+        op_code = instrs[eip]
 
         match op_code:
-            case 1:
-                input_1, input_2, output = prog[eip+1:eip+4]
-                prog[output] = prog[input_1] + prog[input_2]
+            case 1 | 2:
+                val_1 = instrs[instrs[eip+1]]
+                val_2 = instrs[instrs[eip+2]]
+                res = 0
+                if op_code == 1:
+                    res = val_1 + val_2
+                elif op_code == 2:
+                    res = val_1 * val_2
+                instrs[instrs[eip+3]] = res
                 eip += 4
-
-            case 2:
-                input_1, input_2, output = prog[eip+1:eip+4]
-                prog[output] = prog[input_1] * prog[input_2]
-                eip += 4
-
             case 99:
                 finished = True
 
-    if prog[0] == 19690720:
-        print(str(noun) + str(verb))
-        return True
-    else:
-        return False
+    return instrs
 
 with open('input.txt') as f:
-    prog = [int(x) for x in f.read().strip().split(',')]
+    instrs = [int(x) for x in f.read().strip().split(',')]
 
-for i in range(0, len(prog)):
-    for j in range(0, len(prog)):
-        run(prog, i, j)
+instr_dict = {}
+for i, instr in enumerate(instrs):
+    instr_dict[i] = instr
+
+result_dict = run(copy.deepcopy(instr_dict), 12, 2)
+
+found = False
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        result_dict = run(copy.deepcopy(instr_dict), noun, verb)
+        if result_dict[0] == 19690720:
+            print(noun*100+verb)
+            found = True
+            break
+
+    if found:
+        break
