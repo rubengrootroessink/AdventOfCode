662bbde3ffed76e1bd5cc0a9d3230078975e0600
s1468642 <ruben331@gmail.com>
2024-12-26 23:51:51 +0100

    Updated 2019, Day 05

--- 2019/05/1.py
+++ 2019/05/1.py
@@ -1,52 +1,74 @@
 import heapq
-import copy
 
-def run(prog_instrs, input_vars):
-    prog = copy.deepcopy(prog_instrs)
-    
-    input_data = input_vars
+def resolve(mode, offset, instrs):
+    if mode == 0:
+        return instrs[instrs[offset]]
+    else:
+        return instrs[offset]
+
+def run(instrs, inpt):
     eip = 0
 
+    output = []
+    
     finished = False
     while not finished:
-        instr = prog[eip]
+        op_num = instrs[eip]
+        op_code = op_num % 100
 
-        op_code = instr % 100
-        
-        mode_param_3 = instr // 10000
-        mode_param_2 = (instr // 1000) % 10
-        mode_param_1 = (instr // 100) % 10
+        modes = (op_num - op_code) // 100
+        mode_1 = modes % 10
+        mode_2 = ((modes - mode_1) // 10) % 10
+        mode_3 = ((modes - mode_1 - mode_2 * 10) // 100) % 100
+
+        eip_jumps = 2
 
         match op_code:
-            case 1 | 2: # Add or multiply
-                param_1, param_2, param_3 = prog[eip+1:eip+4]
+            case 1 | 2:
 
-                param_1 = param_1 if mode_param_1 == 1 else prog[param_1]
-                param_2 = param_2 if mode_param_2 == 1 else prog[param_2]
+                val_1 = resolve(mode_1, eip+1, instrs)
+                val_2 = resolve(mode_2, eip+2, instrs)
 
-                if op_code == 1:
-                    prog[param_3] = param_1 + param_2
-                else:
-                    prog[param_3] = param_1 * param_2
+                res = 0
                 
-                eip += 4
-
-            case 3 | 4: # Input or output
-                param_1 = prog[eip+1]
-
-                if op_code == 3:
-                    next_val = heapq.heappop(input_data)
-                    prog[param_1] = next_val
+                if op_code == 1:
+                    res = val_1 + val_2
+                elif op_code == 2:
+                    res = val_1 * val_2
+                
+                if mode_3 == 0:
+                    instrs[instrs[eip+3]] = res
                 else:
-                    output = param_1 if mode_param_1 == 1 else prog[param_1]
-                    print(output)
+                    instrs[eip+3] = res
+                
+                eip_jumps += 2
 
-                eip += 2
+            case 3:
+                
+                if mode_1 == 0:
+                    instrs[instrs[eip+1]] = heapq.heappop(inpt)
+                else:
+                    instrs[eip+1] = heapq.heappop(inpt)
+                    
+            case 4:
+
+                val_1 = resolve(mode_1, eip+1, instrs)
+                output.append(val_1)
 
             case 99:
+
                 finished = True
 
-with open('input.txt') as f:
-    prog = [int(x) for x in f.read().strip().split(',')]
+        eip += eip_jumps
 
-run(prog, [1])
+    return output
+
+with open('input.txt') as f:
+    instrs = [int(x) for x in f.read().strip().split(',')]
+
+instr_dict = {}
+for i, instr in enumerate(instrs):
+    instr_dict[i] = instr
+
+outputs = run(instr_dict, [1])
+print(outputs[-1])
--- 2019/05/2.py
+++ 2019/05/2.py
@@ -1,68 +1,92 @@
 import heapq
-import copy
 
-def run(prog_instrs, input_vars):
-    prog = copy.deepcopy(prog_instrs)
-    
-    input_data = input_vars
+def resolve(mode, offset, instrs):
+    if mode == 0:
+        return instrs[instrs[offset]]
+    else:
+        return instrs[offset]
+
+def run(instrs, inpt):
     eip = 0
 
+    output = []
+    
     finished = False
     while not finished:
-        instr = prog[eip]
+        op_num = instrs[eip]
+        op_code = op_num % 100
 
-        op_code = instr % 100
-        
-        mode_param_3 = instr // 10000
-        mode_param_2 = (instr // 1000) % 10
-        mode_param_1 = (instr // 100) % 10
+        modes = (op_num - op_code) // 100
+        mode_1 = modes % 10
+        mode_2 = ((modes - mode_1) // 10) % 10
+        mode_3 = ((modes - mode_1 - mode_2 * 10) // 100) % 100
+
+        eip_jumps = 2
 
         match op_code:
-            case 1 | 2 | 7 | 8: # Add, multiply, less than, equals
-                param_1, param_2, param_3 = prog[eip+1:eip+4]
+            case 1 | 2 | 7 | 8:
 
-                param_1 = param_1 if mode_param_1 == 1 else prog[param_1]
-                param_2 = param_2 if mode_param_2 == 1 else prog[param_2]
+                val_1 = resolve(mode_1, eip+1, instrs)
+                val_2 = resolve(mode_2, eip+2, instrs)
 
-                match op_code:
-                    case 1:
-                        prog[param_3] = param_1 + param_2
-                    case 2:
-                        prog[param_3] = param_1 * param_2
-                    case 7:
-                        prog[param_3] = 1 if param_1 < param_2 else 0
-                    case 8:
-                        prog[param_3] = 1 if param_1 == param_2 else 0
+                res = 0
                 
-                eip += 4
-
-            case 3 | 4: # Input or output
-                param_1 = prog[eip+1]
-
-                if op_code == 3:
-                    next_val = heapq.heappop(input_data)
-                    prog[param_1] = next_val
-                else:
-                    output = param_1 if mode_param_1 == 1 else prog[param_1]
-                    print(output)
-
-                eip += 2
-
-            case 5 | 6: # Jump if (true|false)
-                param_1, param_2 = prog[eip+1:eip+3]
+                if op_code == 1:
+                    res = val_1 + val_2
+                elif op_code == 2:
+                    res = val_1 * val_2
+                elif op_code == 7:
+                    res = 1 if val_1 < val_2 else 0
+                elif op_code == 8:
+                    res = 1 if val_1 == val_2 else 0
                 
-                param_1 = param_1 if mode_param_1 == 1 else prog[param_1]
-                param_2 = param_2 if mode_param_2 == 1 else prog[param_2]
-
-                if (op_code == 5 and param_1 != 0) or (op_code == 6 and param_1 == 0):
-                    eip = param_2
+                if mode_3 == 0:
+                    instrs[instrs[eip+3]] = res
                 else:
-                    eip += 3
+                    instrs[eip+3] = res
+                
+                eip_jumps += 2
+
+            case 3:
+                
+                if mode_1 == 0:
+                    instrs[instrs[eip+1]] = heapq.heappop(inpt)
+                else:
+                    instrs[eip+1] = heapq.heappop(inpt)
+                    
+            case 4:
+
+                val_1 = resolve(mode_1, eip+1, instrs)
+                output.append(val_1)
+
+            case 5 | 6:
+                
+                val_1 = resolve(mode_1, eip+1, instrs)
+                val_2 = resolve(mode_2, eip+2, instrs)
+
+                if op_code == 5 and val_1 != 0:
+                    eip = val_2
+                    eip_jumps = 0
+                elif op_code == 6 and val_1 == 0:
+                    eip = val_2
+                    eip_jumps = 0
+                else:
+                    eip_jumps += 1
 
             case 99:
+
                 finished = True
 
-with open('input.txt') as f:
-    prog = [int(x) for x in f.read().strip().split(',')]
+        eip += eip_jumps
 
-run(prog, [5])
+    return output
+
+with open('input.txt') as f:
+    instrs = [int(x) for x in f.read().strip().split(',')]
+
+instr_dict = {}
+for i, instr in enumerate(instrs):
+    instr_dict[i] = instr
+
+outputs = run(instr_dict, [5])
+print(outputs[-1])
