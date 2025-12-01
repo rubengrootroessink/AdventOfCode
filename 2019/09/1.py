121576d18bb34b85a53aebe727188f1a203bc1d3
s1468642 <ruben331@gmail.com>
2025-12-01 23:40:58 +0100

    Updated 2019, Day 09

--- 2019/09/1.py
+++ 2019/09/1.py
@@ -1,105 +1,102 @@
 from collections import defaultdict
-from itertools import permutations
-import heapq
 
-def resolve(mode, offset, rel_base, instrs):
-    if mode == 0:
-        return instrs[instrs[offset]]
-    elif mode == 1:
-        return instrs[offset]
-    elif mode == 2:
-        return instrs[rel_base+instrs[offset]]
+class Computer:
+    def __init__(self, program, input_data):
+        self.program = program        
+        self.eip = 0
+        self.rel_base = 0
+        self.input_list = input_data
 
-def run(instrs, inpt):
-    eip = 0
+    def parse_opcode(self, opcode):
+        opcode = str(opcode).zfill(5)
+        modes = [int(x) for x in opcode[0:3]][::-1]
+        opcode = int(opcode[3:5])
 
-    rel_base = 0
-    output = []
-    
-    finished = False
-    while not finished:
-        op_num = instrs[eip]
-        op_code = op_num % 100
-
-        modes = (op_num - op_code) // 100
-        mode_1 = modes % 10
-        mode_2 = ((modes - mode_1) // 10) % 10
-        mode_3 = ((modes - mode_1 - mode_2 * 10) // 100) % 100
-
-        eip_jumps = 2
-
-        match op_code:
+        match opcode:
             case 1 | 2 | 7 | 8:
-
-                val_1 = resolve(mode_1, eip+1, rel_base, instrs)
-                val_2 = resolve(mode_2, eip+2, rel_base, instrs)
-
-                res = 0
-                
-                if op_code == 1:
-                    res = val_1 + val_2
-                elif op_code == 2:
-                    res = val_1 * val_2
-                elif op_code == 7:
-                    res = 1 if val_1 < val_2 else 0
-                elif op_code == 8:
-                    res = 1 if val_1 == val_2 else 0
-                
-                if mode_3 == 0:
-                    instrs[instrs[eip+3]] = res
-                elif mode_3 == 1:
-                    instrs[eip+3] = res
-                elif mode_3 == 2:
-                    instrs[rel_base+instrs[eip+3]] = res
-                
-                eip_jumps += 2
-
-            case 3:
-                
-                if mode_1 == 0:
-                    instrs[instrs[eip+1]] = heapq.heappop(inpt)
-                elif mode_1 == 1:
-                    instrs[eip+1] = heapq.heappop(inpt)
-                elif mode_1 == 2:
-                    instrs[rel_base+instrs[eip+1]] = heapq.heappop(inpt)
-                    
-            case 4:
-
-                val_1 = resolve(mode_1, eip+1, rel_base, instrs)
-                output.append(val_1)
-
+                return opcode, modes
+            case 3 | 4 | 9:
+                return opcode, modes[0:1]
             case 5 | 6:
-                
-                val_1 = resolve(mode_1, eip+1, rel_base, instrs)
-                val_2 = resolve(mode_2, eip+2, rel_base, instrs)
-
-                if op_code == 5 and val_1 != 0:
-                    eip = val_2
-                    eip_jumps = 0
-                elif op_code == 6 and val_1 == 0:
-                    eip = val_2
-                    eip_jumps = 0
-                else:
-                    eip_jumps += 1
-
-            case 9:
-
-                val_1 = resolve(mode_1, eip+1, rel_base, instrs)
-                rel_base += val_1
-
+                return opcode, modes[0:2]
             case 99:
+                return opcode, []
+    
+    def reg_value(self, reg, mode, opcode, reg_num):
+        match (mode, opcode, reg_num):
+            case (0, _, _): return self.program[reg]
+            case (1, _, _): return reg
+            case (2, _, _): return self.program[reg] + self.rel_base
 
-                finished = True
+    # Opcode 1
+    def add(self, reg_1, reg_2, reg_3):
+        self.program[reg_3] = self.program[reg_1] + self.program[reg_2]
+        self.eip += 4
 
-        eip += eip_jumps
+    # Opcode 2
+    def mul(self, reg_1, reg_2, reg_3):
+        self.program[reg_3] = self.program[reg_1] * self.program[reg_2]
+        self.eip += 4
 
-    return output
+    # Opcode 3
+    def input(self, reg_1):
+        self.program[reg_1] = self.input_list[0]
+        self.eip += 2
+
+    # Opcode 4
+    def output(self, reg_1):
+        print(self.program[reg_1])
+        self.eip += 2
+
+    # Opcode 5
+    def jit(self, reg_1, reg_2):
+        if self.program[reg_1] != 0: self.eip = self.program[reg_2]
+        else: self.eip += 3
+
+    # Opcode 6
+    def jif(self, reg_1, reg_2):
+        if self.program[reg_1] == 0: self.eip = self.program[reg_2]
+        else: self.eip += 3
+
+    # Opcode 7
+    def lt(self, reg_1, reg_2, reg_3):
+        self.program[reg_3] = int(self.program[reg_1] < self.program[reg_2])
+        self.eip += 4
+
+    # Opcode 8
+    def eq(self, reg_1, reg_2, reg_3):
+        self.program[reg_3] = int(self.program[reg_1] == self.program[reg_2])
+        self.eip += 4
+
+    # Opcode 9
+    def adj_rel(self, reg_1):
+        self.rel_base += self.program[reg_1]
+        self.eip += 2
+
+    def run(self):
+        opcode, modes = self.parse_opcode(self.program[self.eip])
+        while opcode != 99:
+            regs = [self.reg_value(self.eip+1+i, modes[i], opcode, i) for i in range(len(modes))]
+
+            match opcode:
+                case 1: self.add(regs[0], regs[1], regs[2])
+                case 2: self.mul(regs[0], regs[1], regs[2])
+                case 3: self.input(regs[0])
+                case 4: self.output(regs[0])
+                case 5: self.jit(regs[0], regs[1])
+                case 6: self.jif(regs[0], regs[1])
+                case 7: self.lt(regs[0], regs[1], regs[2])
+                case 8: self.eq(regs[0], regs[1], regs[2])
+                case 9: self.adj_rel(regs[0])
+        
+            opcode, modes = self.parse_opcode(self.program[self.eip])
 
 with open('input.txt') as f:
-    instrs = [int(x) for x in f.read().strip().split(',')]
+    data = [int(x) for x in f.read().strip().split(',')]
 
-instr_dict = defaultdict(int)
-for i, instr in enumerate(instrs):
-    instr_dict[i] = instr
+program = defaultdict(int)
+for i, v in enumerate(data):
+    program[i] = v
 
-print(run(instr_dict, [1])[0])
+comp = Computer(program, [1])
+comp.run()
